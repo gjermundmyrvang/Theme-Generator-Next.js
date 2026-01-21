b357c6b497551d275040f0b3bccf180e63479c1c
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-01-21 12:07:43 +0100

    create index file for all custom icons

---
+++ components/icons/index.ts
@@ -0,0 +1,7 @@
+export * from "./CheckIcon";
+export * from "./ColorIcon";
+export * from "./InfoIcon";
+export * from "./LaptopIcon";
+export * from "./MoonIcon";
+export * from "./SunIcon";
+export * from "./TrashIcon";
